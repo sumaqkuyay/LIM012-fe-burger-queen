87ed2dcf15075aa82634cf015d544041d020d569
sumaqkuyay <sabina.canchumani.huaman@gmail.com>
2020-07-22 14:12:49 -0500

    agregando nuevas reglas AL ESLINT para extensiones .js .jsx y otra regla para una linea larga de codigo en react jsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,7 @@ module.exports = {
     'react',
   ],
   rules: {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    'react/jsx-one-expression-per-line': 'off'
   },
 };
