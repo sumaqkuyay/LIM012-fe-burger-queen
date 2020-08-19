19dde9ce67f8e4d0da6cf92e1dc5895981f188cb
roxanaguerra <sguerraapaza@gmail.com>
2020-08-19 13:46:22 -0500

    creación de coleccion y lectura en firebase

--- src/controller/firestore.js
+++ src/controller/firestore.js
@@ -1,9 +1,32 @@
 import firebase from './main';
 
-const addOrder = (table) => {
-  const firestore = firebase.firestore();
-  firestore.collection('addOrder').add({
-    table,
-  });
+// COLECCION EN FIRESTORE - ADD ORDER
+const collectionOrder = () => firebase.firestore().collection('addOrder');
+
+// AGREGAR DOCS A LA COLECCION
+const addOrder = (arrayOrder) => {
+  collectionOrder().add({ arrayOrder })
+    .then((docRef) => {
+      console.log('Document written with ID: ', docRef.id);
+    })
+    .catch((error) => {
+      console.error('Error adding document: ', error);
+    });
 };
-export default addOrder;
+
+// LEER LOS DOCS DE LA COLECCION
+const getOrder = () => {
+  collectionOrder().onSnapshot((query) => {
+    const docs = [];
+    query.forEach((order) => {
+      docs.push({ ...order.data(), id: order.id });
+    });
+    console.log(docs);
+  });
+  // callback(docs);
+};
+
+export default {
+  addOrder,
+  getOrder,
+};
--- src/controller/main.js
+++ src/controller/main.js
@@ -16,4 +16,3 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 export default firebase;
-
