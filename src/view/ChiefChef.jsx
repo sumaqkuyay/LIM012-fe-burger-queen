72e82a14d3a78d9c44a2e2f55d57b79176be7b67
sumaqkuyay <sabina.canchumani.huaman@gmail.com>
2020-08-25 14:57:21 -0500

    ya funcionan los botones usando history.push

--- src/view/ChiefChef.jsx
+++ src/view/ChiefChef.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import NavigationBar from '../components/NavigationBar';
 import OrderCard from '../components/OrderCard';
@@ -6,6 +7,7 @@ import firestore from '../controller/firestore';
 
 const ChiefChef = () => {
   const [orders, getOrders] = useState([]);
+  const history = useHistory();
 
   useEffect(() => {
     // getOrderFirestore();
@@ -17,7 +19,7 @@ const ChiefChef = () => {
 
   return (
     <>
-      <Header name="JEFE DE COCINA" />
+      <Header name="JEFE DE COCINA" history={history} />
       <NavigationBar />
       {
         orders.map((order) => (
--- src/view/Welcome.jsx
+++ src/view/Welcome.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import logo from '../images/logo-principal.png';
 import MainButton from '../components/MainButton';
 
-const Welcome = () => (
-  <div className="component-welcome">
-    <img className="logo-welcome" src={logo} alt="Mikuy Burger" />
-    <div className="prueba">
-      <MainButton classbtn="btn btn-welcome" name="Mesero" reference="/mozo" />
-      <MainButton classbtn="btn btn-welcome" name="Jefe de Cocina" reference="/jefecocina" />
+const Welcome = () => {
+  const history = useHistory();
+  return (
+    <div className="component-welcome">
+      <img className="logo-welcome" src={logo} alt="Mikuy Burger" />
+      <div className="prueba">
+        <MainButton classbtn="btn btn-welcome" name="Mesero" reference="/mozo" history={history} />
+        <MainButton classbtn="btn btn-welcome" name="Jefe de Cocina" reference="/jefecocina" history={history} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default Welcome;
