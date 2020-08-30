d6fa382cacb93aa7f29ebd1cf19116600f05da6a
sumaqkuyay <sabina.canchumani.huaman@gmail.com>
2020-08-30 01:10:33 -0500

    Updates

---
+++ precache-manifest.434ae4297dab9e14fa91748bfc22c45c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e2132b0066e75846f35ca20c823aa9",
+    "url": "/LIM012-fe-burger-queen/index.html"
+  },
+  {
+    "revision": "6e9c4beb1242d4e07ee7",
+    "url": "/LIM012-fe-burger-queen/static/css/main.bbea8362.chunk.css"
+  },
+  {
+    "revision": "4608464414b311b18d40",
+    "url": "/LIM012-fe-burger-queen/static/js/2.239c5214.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/LIM012-fe-burger-queen/static/js/2.239c5214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e9c4beb1242d4e07ee7",
+    "url": "/LIM012-fe-burger-queen/static/js/main.d667bff2.chunk.js"
+  },
+  {
+    "revision": "2badaad4235dafd2f1b9",
+    "url": "/LIM012-fe-burger-queen/static/js/runtime-main.a5b50a99.js"
+  },
+  {
+    "revision": "1d66bc4e49bd4d3350940df602fce391",
+    "url": "/LIM012-fe-burger-queen/static/media/fondo-principal.1d66bc4e.jpg"
+  },
+  {
+    "revision": "50677615ef4f9b2b9bde03963715bcf9",
+    "url": "/LIM012-fe-burger-queen/static/media/logo-principal.50677615.png"
+  }
+]);
